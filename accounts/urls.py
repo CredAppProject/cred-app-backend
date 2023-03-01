180587223d0a01edb7a5949ada82eac54b27e23b
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-03-01 20:12:23 +0530

    created apis for login and refreshing the jwt token

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from accounts.views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path("api/v1",hello)
+    path("api/auth/v1/login",TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path("api/auth/v1/login/refresh",TokenRefreshView.as_view(), name='token_refresh'),
 ]
