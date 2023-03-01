930f700236e480b647d909cca152aaf40ba0ea40
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-03-01 23:17:23 +0530

    url for acc create added

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,10 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from accounts.views import UserView
 
 urlpatterns = [
     path("api/auth/v1/login",TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path("api/auth/v1/login/refresh",TokenRefreshView.as_view(), name='token_refresh'),
+    path("api/auth/v1/create",UserView.as_view()),
 ]
