baabbb067bfe2e3bc18a5ed1bcb01429c7373ef6
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-03-02 10:30:25 +0530

    added permission class

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 from accounts.models import User
 from accounts.serializer import UserSerializer
 
 
 class UserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
