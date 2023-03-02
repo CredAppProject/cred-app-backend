feb18e22b5e6031940b8defad0ae2a3608b27cbd
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-03-02 11:17:14 +0530

    created user get,put,delete apis

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,10 +4,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from accounts.views import UserView
+from accounts.views import UserRegistrationAPI,UserProfileAPI
 
 urlpatterns = [
     path("api/auth/v1/login",TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path("api/auth/v1/login/refresh",TokenRefreshView.as_view(), name='token_refresh'),
-    path("api/auth/v1/create",UserView.as_view()),
+    path("api/auth/v1/create",UserRegistrationAPI.as_view()),
+    path("api/auth/v1/<str:pk>",UserProfileAPI.as_view(),name="user-profile"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny,IsAuthenticated
 from accounts.models import User
 from accounts.serializer import UserSerializer
 
@@ -11,4 +11,18 @@ class UserRegistrationAPI(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
+class UserProfileAPI(generics.RetrieveUpdateDestroyAPIView):
+    """API Endpoint for fetching,updating,deleting the current user."""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+    lookup_url_kwarg = 'pk'
+    lookup_field = 'id'
 
+    # def get(self,request):
+    #     queryset = self.filter_queryset(self.get_queryset())
+    #     # make sure to catch 404's below
+    #     obj = queryset.get(pk=self.request.user.id)
+    #     print(obj)
+    #     self.check_object_permissions(self.request, obj)
+    #     return obj
