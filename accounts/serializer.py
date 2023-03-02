4609bc8057c2736ca75529785599aac963c0f80d
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-03-02 10:52:13 +0530

    bugs fixed while creating a user

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -10,6 +10,10 @@ class UserSerializer(serializers.ModelSerializer):
 
   def create(self, validated_data):
     """To create a new auth.user model"""
-    user = User.objects.create_user(**validated_data)
+    user = User.objects.create_user(validated_data['email'],validated_data['name'],validated_data['phone'],validated_data['password'])
+    user.profile_picture = validated_data['profile_picture']
+    user.company = validated_data['company']
+    user.job = validated_data['job']
     user.save()
     return user
+  
