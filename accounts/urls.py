e563c2b77c4b2d7cee62292733c521f59f6896c6
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-03-02 20:29:02 +0530

    new url path for password update

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,TokenBlacklistView
 )
-from accounts.views import UserRegistrationAPI,UserProfileAPI,UserList
+from accounts.views import UserRegistrationAPI,UserProfileAPI,UserList,UserPasswordUpdate
 
 urlpatterns = [
     path("api/auth/v1/login",TokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -12,7 +12,9 @@ urlpatterns = [
     path('api/auth/v1/logout', TokenBlacklistView.as_view(), name='token_blacklist'),
 
     path("api/auth/v1/create",UserRegistrationAPI.as_view()),
-    path("api/auth/v1/<str:pk>",UserProfileAPI.as_view(),name="user-profile"),
+    path("api/auth/v1/profile/<str:pk>",UserProfileAPI.as_view(),name="user-profile"),
+
+    path("api/auth/v1/change_password",UserPasswordUpdate.as_view(),name="change-password"),
 
     #development purpose
     path("api/auth/users",UserList.as_view()),
