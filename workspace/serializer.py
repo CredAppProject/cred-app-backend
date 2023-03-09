31359fc4402239cac9efa6b4085ea91a14105bfc
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-03-09 19:28:37 +0530

    created the serializer for workspace model

---
+++ workspace/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from workspace.models import Workspace
+
+class WorkspaceSerializer(serializers.ModelSerializer):
+  class Meta:
+      model = Workspace
+      fields = "__all__"
+      read_only_fields  = ('workspace_id',)
