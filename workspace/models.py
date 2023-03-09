9ef2ca42c7dbee8083fcd5119350e4662a09d301
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-03-09 19:26:03 +0530

    created the models.py

--- workspace/models.py
+++ workspace/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Workspace(models.Model):
+    workspace_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    workspace_name = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.workspace_name
