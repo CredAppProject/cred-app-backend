e41a4e716e993ed306e1f85fe9a9de32d68bb687
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-03-09 22:47:11 +0530

    added new variable to workspace db

--- workspace/models.py
+++ workspace/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from accounts.models import User
 import uuid
 
 class Workspace(models.Model):
-    workspace_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    workspace_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True,primary_key=True)
     workspace_name = models.CharField(max_length=50)
     category = models.CharField(max_length=50)
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.workspace_name
