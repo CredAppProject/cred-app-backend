ffa98ddbc6830232d5937beab53f685faad34d0a
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-03-09 19:38:58 +0530

    created the get and post api routes for workspace

--- workspace/views.py
+++ workspace/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from workspace.models import Workspace
+from workspace.serializer import WorkspaceSerializer
 
 # Create your views here.
+class CustomUserCreation(generics.ListCreateAPIView):
+    """API endpoint to create and list all the workspaces"""
+    queryset = Workspace.objects.all()
+    permission_classes = (IsAuthenticated)
+    serializer_class = WorkspaceSerializer
