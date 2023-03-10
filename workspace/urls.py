ab825bf56e920689278d77eb5ee823faf2424cd4
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-03-10 11:36:03 +0530

    added new url for workspace individual get,update,delete endpoints

--- workspace/urls.py
+++ workspace/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from workspace.views import WorkspaceListCreateView
+from workspace.views import WorkspaceListCreateView,WorkspaceGetUpdateDestroyView
 
 urlpatterns = [
     path("api/v1",WorkspaceListCreateView.as_view(), name='workspace_list_create'),
+    path("api/v1/<str:id>",WorkspaceGetUpdateDestroyView.as_view(), name='workspace_update_delete'),
 ]
