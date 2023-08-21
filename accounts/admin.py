ec9339252d092d65c7cb61a8371e17c698f517d1
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-08-21 14:05:02 +0530

    Updated model admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.models import Group
 # Register your models here.
 
-admin.site.site_header = "Cred Admin"
-admin.site.register(User)
+admin.site.site_header = "Cred App Dashboard"
+
+class YourModelAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'phone','profile_picture', 'date_joined', 'company', "job")
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(id=request.user.id)  # Assuming there's a ForeignKey to User in YourModel.
+
+admin.site.register(User, YourModelAdmin)
--- cred/settings.py
+++ cred/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'workspace',
     'rest_framework',
-    'rest_framework_simplejwt.token_blacklist',
+    # 'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
